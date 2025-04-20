f3a5f56e7c3195de7a3145a60ffbba6e14d096d7
isaias <isgb_codigo@hotmail.com>
2025-04-20 01:24:48 -0600

    botones de cards task

--- src/components/CardsList/CardTasks/ButtonsCardTask.jsx
+++ src/components/CardsList/CardTasks/ButtonsCardTask.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import { faAnglesDown, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import '../../../styles/buttonscardtask.css'
+import React from "react";
 
 export const ButtonsCardTask = () => {
   return (
-    <div className="col-2 container-buttons-card-tasks">
-        <div>BORR</div>
-        <div>DESP</div>
+    <div className="col-2 container-buttons-card-tasks d-flex flex-column justify-content-between align-items-center">
+      
+      <div className="iconButton">
+        <FontAwesomeIcon icon={faTrash} />
       </div>
-  )
-}
+      
+      <div className="iconButton">
+        <FontAwesomeIcon icon={faAnglesDown} />
+      </div>
+      
+    </div>
+  );
+};
