80ca3759740319d95f785d20cdbac99e934d6a84
isaias <isaias.gaona@zentro.com.mx>
2025-04-21 16:52:18 -0600

    diseño card task

--- src/components/CardsList/CardTasks/CardInformation.jsx
+++ src/components/CardsList/CardTasks/CardInformation.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const CardInformation = () => {
   return (
     <div className="col-7 card-information">
-    <h4>Add Your Task List</h4>
+    <h4>Add Your Task List</h4> 
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
       odio explicabo ducimus
--- src/components/CardsList/CardTasks/TaskItem/TaskItem.jsx
+++ src/components/CardsList/CardTasks/TaskItem/TaskItem.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import "../../../../styles/TaskItem.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSquare } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export const TaskItem = () => {
   return (
-    <section className="task row justify-content-between card-task">
-      <div className="iconButton col-2">
-      <FontAwesomeIcon icon={faSquare} />
+    <section className="task row justify-content-between card-task me-3">
+      
+      <div className="iconButton col-2 d-flex justify-content-center align-items-center">
+        <FontAwesomeIcon 
+            icon={faCheck} 
+            className="icon-checkbox"
+          />
       </div>
 
-      <p className="col-10">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      </p>
+      <div className="col-8 description-task">
+         <h4>Add Your Task List</h4> 
+      </div>
+
+      <div className="iconButton col-2 d-flex justify-content-center align-items-center pe-0">
+        <FontAwesomeIcon 
+            icon={faTrash}
+            className="icon-trash-task"
+        />
+      </div>
+      
     </section>
   );
 };
