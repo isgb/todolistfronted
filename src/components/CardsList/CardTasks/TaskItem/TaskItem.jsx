c4b331966f9e0c0eea674a7bfe4203eb7329c750
isaias <isgb_codigo@hotmail.com>
2025-04-21 00:03:30 -0600

    creando task cards

--- src/components/CardsList/CardTasks/TaskItem/TaskItem.jsx
+++ src/components/CardsList/CardTasks/TaskItem/TaskItem.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "../../../../styles/TaskItem.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSquarePollVertical } from "@fortawesome/free-solid-svg-icons";
+import { faSquare } from "@fortawesome/free-solid-svg-icons";
 
 export const TaskItem = () => {
   return (
     <section className="task row justify-content-between card-task">
       <div className="iconButton col-2">
-        <FontAwesomeIcon icon={faSquarePollVertical} />
+      <FontAwesomeIcon icon={faSquare} />
       </div>
 
       <p className="col-10">
