e03e64ce9c912e3d7590df817ed6b8989d90521c
isaias <isaias.gaona@zentro.com.mx>
2025-04-24 16:17:34 -0600

    handle Change to input task

--- src/components/CardsList/CardTasks/TaskItem/TaskItem.jsx
+++ src/components/CardsList/CardTasks/TaskItem/TaskItem.jsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../../../styles/TaskItem.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faTrash } from "@fortawesome/free-solid-svg-icons";
 
-export const TaskItem = ({task, setTasksList, tasks, indexItem}) => {
+export const TaskItem = ({ task, setTasksList, tasks, indexItem }) => {
 
-  const {description, isCompleted} = task;
+  const { description, isCompleted } = task;
+  const [changeToInput, setChangeToInput] = useState(false);
 
   const handleChangeIsCompleted = async (check) => {
-    const tasksModified = tasks.map((task,index) => {
-        return index === indexItem ? {...task, isCompleted : check} : task
+    const tasksModified = tasks.map((task, index) => {
+      return index === indexItem ? { ...task, isCompleted: check } : task
     })
     setTasksList(tasksModified)
   }
 
   const handleDeleteTask = async () => {
-    const tasksModified = tasks.filter((task,index) => {
-        return index !== indexItem
+    const tasksModified = tasks.filter((task, index) => {
+      return index !== indexItem
+    })
+    setTasksList(tasksModified)
+  }
+
+  const handleChangeDescription = async (description) => {
+    const tasksModified = tasks.map((task, index) => {
+      return index === indexItem ? { ...task, description: description } : task
     })
     setTasksList(tasksModified)
   }
@@ -27,21 +35,36 @@ export const TaskItem = ({task, setTasksList, tasks, indexItem}) => {
         <FontAwesomeIcon
           icon={faCheck}
           onClick={() => handleChangeIsCompleted(!isCompleted)}
-          className={`icon-checkbox ${isCompleted ? "color-check-change" : "color-check-principal" }`} 
+          className={`icon-checkbox ${isCompleted ? "color-check-change" : "color-check-principal"}`}
         />
       </div>
 
       <div className="col-8 description-task">
-        <h4 className={isCompleted ? 'text-decoration-line-through' : ''}>
-          {description || 'Add an description'}
-        </h4>
+        {changeToInput ? (
+          <input
+            type="text"
+            className="form-control"
+            defaultValue={description}
+            onBlur={(e) => {
+              handleChangeDescription(e.target.value);
+              setChangeToInput(!changeToInput);
+            }}
+          />
+        ) : (
+          <h4
+            className={isCompleted ? 'text-decoration-line-through' : ''}
+            onClick={() => setChangeToInput(!changeToInput)}
+          >
+            {description || 'Add an description'}
+          </h4>
+        )}
       </div>
 
       <div className="iconButton col-2 d-flex justify-content-center align-items-center pe-0">
-        <FontAwesomeIcon 
-            icon={faTrash} 
-            className="icon-trash-task" 
-            onClick={() => handleDeleteTask()}
+        <FontAwesomeIcon
+          icon={faTrash}
+          className="icon-trash-task"
+          onClick={() => handleDeleteTask()}
         />
       </div>
     </section>
