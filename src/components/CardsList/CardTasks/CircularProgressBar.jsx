cf427d0b26968a6e9417dd25ca06abfb9f5b529c
isaias <isgb_codigo@hotmail.com>
2025-04-26 23:37:29 -0600

    frontend parte

--- src/components/CardsList/CardTasks/CircularProgressBar.jsx
+++ src/components/CardsList/CardTasks/CircularProgressBar.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
 export const CircularProgressBar = ({percentage}) => {
-
-  // console.log("percentage",percentage)
   return (
     <div className="col-3 porcentaje d-flex justify-content-center align-content-center">
         <div className="content-circularprogressbar d-flex align-items-center justify-content-center">
