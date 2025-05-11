f6ba4412147a2f23ab4c4c733e1919dc5016f4f5
isaias <isgb_codigo@hotmail.com>
2025-05-10 23:59:43 -0600

    version prod

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3970/api',
+    baseURL: 'https://todolistbackend-eight.vercel.app/api',
 });
 
 export default instance;
